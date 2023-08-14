9dd7a7cdd199bee0c71b8f1e98639d7ea2edfc7c
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-08-14 19:50:34 +0545

    Upgraded links , added cartIcon

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import React, { useState } from 'react'
+import CartIcon from './CartIcon';
 
 const links = [
   { id:1, title: "Home", url:"/"},
@@ -24,19 +25,25 @@ const Menu = () => {
       )}
     
       {/* ADDING LINKS */}
-      <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
-        {links.map((item) => (
-          <Link href={item.url} key={item.id}> {item.title} </Link>
-        ))}
-        {/* ADDING Login LINKS */}
 
-          {!user ? (
-          <Link href="/login">Login</Link>
-          ) : (
-          <Link href="/orders">Orders</Link>
-          )}
-        
-      </div>
+      { open && (
+        <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={()=>setOpen(false)} > {item.title} </Link>
+          ))}
+          {/* ADDING Login LINKS */}
+
+            {!user ? (
+              <Link href="/login" onClick={()=>setOpen(false)} >Login</Link>
+            ) : (
+              <Link href="/orders" onClick={()=>setOpen(false)} >Orders</Link>
+            )}
+            <Link href="/cart" onClick={()=>setOpen(false)} >
+              <CartIcon/>
+            </Link>
+
+        </div>
+      )}
     </div>
   );
 };
