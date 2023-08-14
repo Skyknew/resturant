8a8305d4473bb8ac3f1583ba682051c37a20251a
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-08-14 19:50:55 +0545

    Added Responsive screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
+
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase ">
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 lx:px-40">
+      {/* LEFT SIDE */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">HomePage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/* LOGO */}
-      <div className='text-xl'>
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Resturant</Link>
       </div>
 
       {/* MOBILE MENU */}
-      <div>
-        <Menu/>
+      <div className="md:hidden">
+        <Menu />
       </div>
 
+      {/* RIGHT SIDE */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20}/>
+          <span>123 456 789</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
